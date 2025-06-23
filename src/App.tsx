3bfe19b29fadc8196f22fe62623ee007e5b10276
Suraiya <medsusu713@gmail.com>
2025-06-23 22:27:15 +0000

    update app.tsx and button.tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import InventoryApp from "./components/Inventory-System-ui"; // Update the path if the file is in a different location
 // Update the import path to match the actual filename, e.g.:
